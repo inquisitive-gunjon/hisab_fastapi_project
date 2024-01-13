95607561d0ccf0a80ed14a07082afa5a2943cf48
inquisitive-gunjon <gunjon.cse@gmail.com>
2024-01-13 15:12:57 +0600

    updated database.py file

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base, DeclarativeMeta
 
 SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root@localhost:3306/hishabproject"
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL,echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base: DeclarativeMeta = declarative_base()
